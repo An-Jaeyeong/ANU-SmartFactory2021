68fe2a505ba23e8c0b8761a054b188dabad9d5d2
An-Jaeyeong <65813865+An-Jaeyeong@users.noreply.github.com>
2022-04-29 16:54:32 +0900

    Add files via upload

---
+++ 1.Common/DBHelper.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Oracle.ManagedDataAccess.Client;
+
+namespace Final_FUI
+{
+    class DBHelper
+    {
+        public DBHelper() { }
+
+
+
+        private static OracleConnection conn = null;
+        public static string DBconnString
+        {
+            get;
+            private set;
+        }
+
+        public static bool bDBConnCheck = false;
+
+        private static int errorBoxCount = 0;
+
+
+
+
+        public static OracleConnection DBconn
+        {
+            get
+            {
+                if (!ConnectToDB())
+                {
+                    return null;
+                }
+                return conn;
+            }
+        }
+        public static bool ConnectToDB()
+        {
+            if (conn == null)
+            {
+                DBconnString = string.Format("Data Source=(DESCRIPTION="
+                    + "(ADDRESS_LIST=(ADDRESS=(PROTOCOL=TCP)(HOST=192.168.47.196)"
+                    //+ "(ADDRESS_LIST=(ADDRESS=(PROTOCOL=TCP)(HOST=192.168.35.39)"
+                    + "(PORT=1521)))(CONNECT_DATA= (SERVER = DEDICATED)"
+                    + "(SERVICE_NAME = xe))); User Id = mes; Password = 1234; ");
+                //+ "(SERVICE_NAME = xe))); User Id = pro; Password = pro; "); 
+                conn = new OracleConnection(DBconnString);
+            }
+            try
+            {
+                if (!IsDBConnected)
+                {
+                    conn.Open();
+
+                    if (conn.State == System.Data.ConnectionState.Open)
+                    {
+                        bDBConnCheck = true;
+                    }
+                    else
+                    {
+                        bDBConnCheck = false;
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+                errorBoxCount++;
+                if (errorBoxCount == 1)
+                {
+                    MessageBox.Show(e.Message, "DBHelper - connectToDB");
+                }
+                return false;
+            }
+            return true;
+        }
+        public static bool IsDBConnected
+        {
+            get
+            {
+                if (conn.State != System.Data.ConnectionState.Open)
+                {
+                    return false;
+                }
+                return true;
+            }
+        }
+        public static void Close()
+        {
+            if (IsDBConnected)
+            {
+                DBconn.Close();
+            }
+
+        }
+    }
+}
+
+
---
+++ 1.Common/DBconnection.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Oracle.ManagedDataAccess.Client;
+using System.Data;
+using System.Drawing;
+
+namespace Final_FUI
+{
+    class DBconnection
+    {
+        /// <summary>
+        ///  The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        //static void Main()
+        //{
+        //    Application.SetHighDpiMode(HighDpiMode.SystemAware);
+        //    Application.EnableVisualStyles();
+        //    Application.SetCompatibleTextRenderingDefault(false);
+        //    Application.Run(new Login());
+        //}
+        static public void SetGridDesign(DataGridView Grid)
+        {
+
+            Grid.AlternatingRowsDefaultCellStyle.BackColor = Color.FromArgb(238, 239, 249);
+            Grid.CellBorderStyle = DataGridViewCellBorderStyle.SingleHorizontal;
+            Grid.DefaultCellStyle.SelectionBackColor = Color.DarkTurquoise;
+            Grid.DefaultCellStyle.SelectionForeColor = Color.WhiteSmoke;
+            Grid.BackgroundColor = Color.White;
+            Grid.EnableHeadersVisualStyles = false;
+            Grid.ColumnHeadersBorderStyle = DataGridViewHeaderBorderStyle.None;
+            Grid.ColumnHeadersDefaultCellStyle.BackColor = Color.FromArgb(20, 25, 72);
+            Grid.ColumnHeadersDefaultCellStyle.ForeColor = Color.White;
+            Grid.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.Fill;
+            Grid.Font = new Font("Fixsys", 12, FontStyle.Regular);
+            Grid.ReadOnly = true;
+            Grid.AllowUserToAddRows = false;
+
+        }
+        static public void SetColumnWidth(DataGridView dataGridView, int IndexCol, int widthCol)
+        {
+            dataGridView.Columns[IndexCol].Width = widthCol;
+            
+        }
+
+        static public void DB_Connection(string sql, DataGridView gridView)
+        {
+            OracleDataAdapter adapter = new OracleDataAdapter(sql, DBHelper.DBconn);
+            DataTable data_table = new DataTable();
+            adapter.Fill(data_table);
+            gridView.DataSource = data_table;
+        }
+        /// <summary>
+        /// SELECT 전용
+        /// </summary>
+        /// <param name="sql"></param>
+        /// <returns></returns>
+        static public DataTable DB_Connection(string sql)
+        {
+            OracleDataAdapter adapter = new OracleDataAdapter(sql, DBHelper.DBconn);
+            DataTable data_table = new DataTable();
+            adapter.Fill(data_table);
+            return data_table;
+        }
+        /// <summary>
+        /// INSERT/UPDATE 전용
+        /// </summary>
+        /// <param name="sql"></param>
+        static public void DB_Connection1(string sql)
+        {
+            OracleCommand cmd = new OracleCommand(sql, DBHelper.DBconn);
+            cmd.ExecuteNonQuery();
+
+        }
+    }
+}
